13952b79dce69e1ce2c6a01e84a4479a026c9e8d
Gabriel Thiago <leucides123@gmail.com>
2026-02-15 23:42:48 -0300

    add mais uma função para o descongelamento do nosso objeto com o modelo e preprocessamento salvos modo pkl

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,7 @@ from src.exception import CustomException
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
+
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path) 
@@ -45,6 +46,17 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
             report[list(models.keys())[i]] = test_model_score 
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj) 
 
     except Exception as e:
         raise CustomException(e, sys)
